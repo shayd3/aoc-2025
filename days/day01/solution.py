5de65a41b0c48f64435780facf69c143cd1456f1
shayd3 <ryan.ross215@gmail.com>
2025-12-02 20:15:28 -0500

    day 1 - part 1 complete

--- days/day01/solution.py
+++ days/day01/solution.py
@@ -1,7 +1,32 @@
+'''
+A dial is numbered 0-99
+It round robins (if you go past 99, you restart at 0 and vice versa)
+
+Each line in input starts with either L or R, indicating which direction to go on the dial.
+L decreases the number
+R increases the number
+
+return the count how many times you hit 0 after turning the dial
+'''
+
 def part1(data: str) -> int:
     lines = data.strip().split('\n')
-    # Your solution here
-    return 0
+    position = 50
+    zero_count = 0
+
+    for line in lines:
+        direction = line[0]
+        clicks = int(line[1:])
+
+        # % 100 will round robin
+        if direction == 'L':
+            position = (position - clicks) % 100
+        else: # direction R
+            position = (position + clicks) % 100
+
+        if position == 0:
+            zero_count += 1
+    return zero_count
 
 def part2(data: str) -> int:
     lines = data.strip().split('\n')
